acd1ddd14f1250258c523ba169301a2da26d9811
Forest Gregg <fgregg@uchicago.edu>
2014-11-19 17:53:14 -0600

    packaging material

---
+++ rlr/__init__.py
@@ -0,0 +1,1 @@
+from lr import lr
---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup, Extension
+except ImportError :
+    raise ImportError("setuptools module required, please go to https://pypi.python.org/pypi/setuptools and follow the instructions for installing setuptools")
+
+# from Michael Hoffman's http://www.ebi.ac.uk/~hoffman/software/sunflower/
+
+class NumpyExtension(Extension):
+
+    def __init__(self, *args, **kwargs):
+        Extension.__init__(self, *args, **kwargs)
+
+        self._include_dirs = self.include_dirs
+        del self.include_dirs  # restore overwritten property
+
+    # warning: Extension is a classic class so it's not really read-only
+    @property
+    def include_dirs(self):
+        from numpy import get_include
+
+        return self._include_dirs + [get_include()]
+
+
+setup(
+    name='rlr',
+    url='https://github.com/datamade/rlr',
+    version='1.0',
+    description='A Cython implementation of L2 regularized logistic regression',
+    packages=['rlr'],
+    install_requires=['numpy'],
+    ext_modules=[NumpyExtension('rlr.lr', ['rlr/rlr.c'])],
+    license='The MIT License: http://www.opensource.org/licenses/mit-license.php',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Cython', 
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Information Analysis']
+    )
+
