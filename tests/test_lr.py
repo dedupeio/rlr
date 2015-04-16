e624a7a4c76e4b0f7835c4416c8770147134d771
Forest Gregg <fgregg@uchicago.edu>
2015-04-16 16:09:37 -0500

    broken tests

--- tests/test_lr.py
+++ tests/test_lr.py
@@ -9,7 +9,6 @@ class DataModelTest(unittest.TestCase) :
         examples = numpy.array([1, 0] * 6).reshape(12, 1)
         
 
-        print lr(labels, examples, 0)
 
 
     def test_b_alpha(self) :
@@ -17,15 +16,11 @@ class DataModelTest(unittest.TestCase) :
         examples = numpy.array([1, 0] * 6).reshape(12, 1)
         
 
-        print lr(labels, examples, 0)
-
 
     def test_c_alpha(self) :
         labels = numpy.array([1] * 6 + [1] * 6)
         examples = numpy.array([1, 0] * 6).reshape(12, 1)
         
-        print lr(labels, examples, 0)
-
 
     def test_d_alpha(self) :
         labels = numpy.array([1] * 6 + [0] * 6)
@@ -34,4 +29,3 @@ class DataModelTest(unittest.TestCase) :
         case_weights = numpy.arange(1, 13) * 1./12
         case_weights = numpy.array([0.5] * 12)
 
-        print lr(labels, examples, 0, case_weights)
